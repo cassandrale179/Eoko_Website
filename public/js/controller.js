f164ddd3a90d245b65ef21c2673d66939519912b
Immunity-Project <gkm24@drexel.edu>
2017-03-15 07:35:22 -0400

    created surveys

--- public/js/controller.js
+++ public/js/controller.js
@@ -132,7 +132,7 @@ angular.module('app.controllers', [])
                         displayName: $scope.buildingcode
                     })
 
-                    firebase.database().ref('Buildings').child($scope.buildingcode).child('Admin').
+                    firebase.database().ref('Buildings').child($scope.buildingcode).child('Admin/Users').
                     child(usr.uid).set($scope.info).then(function(success)
                     {
                         
@@ -225,21 +225,68 @@ $scope.LogUser = function()
 
 
 
+.controller('feedbackCtrl', ['$scope','$window','$timeout',
+    // a verbose line seperator between the top construction section and the function for the controller below
 
-.controller('portalPageCtrl', ['$scope','$firebaseArray',
-	// a verbose line seperator between the top construction section and the function for the controller below
+ function($scope,$window,$timeout) {
 
- function($scope,$firebaseArray) {
+ $scope.user = {description:""};
+ $scope.thankyou = false;
+
+ $scope.submitFeeback = function()
+ {
+    if($scope.user.description == "" || $scope.user.description == " ")
+        {
+            $scope.errorpopup = "Please enter some feedback";
+            return;
+        }
+
+    firebase.database().ref("Feedback").push($scope.user.description).then(function(success)
+    {
+        $window.scrollTo(0, 0);
+        $scope.thankyou = true;
+        $timeout(function () {$scope.$apply();});
+        $timeout(function() {$scope.thankyou = false;$window.history.back();}, 3000);
+    });
+    
+ };
+
+}])
+
+
+.controller('constructionCtrl', ['$scope',
+    // a verbose line seperator between the top construction section and the function for the controller below
+
+ function($scope) {
+
+
+}])
+
+
+
+
+
+
+
+
+
+
+.controller('portalPageCtrl', ['$scope','$firebaseArray','$timeout','$window',
+    // a verbose line seperator between the top construction section and the function for the controller below
+
+ function($scope,$firebaseArray,$timeout,$window) {
 
 
 var usr = firebase.auth().currentUser;
 var userRef,eventRef;
+$scope.errorpopup = '';
 
 firebase.auth().onAuthStateChanged(function(user) {
     var usr = firebase.auth().currentUser;
     console.log("user confirmed", usr);
     userRef = firebase.database().ref('Buildings').child(usr.displayName).child('Users');
     eventRef = firebase.database().ref('Buildings').child(usr.displayName).child('UserEvents');
+    adminRef = firebase.database().ref('Buildings').child(usr.displayName).child('Admin');
 
     $scope.userData = $firebaseArray(userRef);
     $scope.userData.$loaded().then(function(x) {
@@ -269,7 +316,7 @@ $scope.userList = [];
 
 
 
-$scope.selection = {tab:"Events"};
+$scope.selection = {tab:"Surveys"};
 
 $scope.selector = function(num)
 {
@@ -320,42 +367,236 @@ $scope.denyUser = function(user)
     }
 };
 
+var weekday = new Array();
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
 
-}])
+        var month = new Array();
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
 
 
-.controller('feedbackCtrl', ['$scope','$window',
-    // a verbose line seperator between the top construction section and the function for the controller below
+$scope.event = {
+    category:"",
+    date: new Date(),
+    location:"",
+    time: "",
+    description:"",
+    title:""
+    };
+$scope.eventsubmitted = false;
+$scope.surveysubmitted = false;
 
- function($scope,$window) {
 
- $scope.user = {description:""};
-
-
- $scope.submitFeeback = function()
- {
-    if($scope.user.description == "" || $scope.user.description == " ")
+$scope.submitEvent = function()
+{
+    console.log("event submit begin");
+    if($scope.event.category == "" || $scope.event.category == " ")
         {
-            $scope.errorpopup = "Please enter some feedback";
+            $scope.errorpopup = "Please enter at least one category";
+            return;
+        }
+        if($scope.event.date == "" || $scope.event.date == " ")
+        {
+            $scope.errorpopup = "Please enter the event date";
+            return;
+        }
+        if($scope.event.location == "" || $scope.event.location == " ")
+        {
+            $scope.errorpopup = "Please enter the event location";
+            return;
+        }
+        if($scope.event.time == "" || $scope.event.time == " ")
+        {
+            $scope.errorpopup = "Please enter the event time";
+            return;
+        }
+        if($scope.event.description == "" || $scope.event.description == " ")
+        {
+            $scope.errorpopup = "Please enter a description";
+            return;
+        }
+        if($scope.event.title == "" || $scope.event.title == " ")
+        {
+            $scope.errorpopup = "Please enter a title";
             return;
         }
 
-    firebase.database().ref("Feedback").push($scope.user.description).then(function(success)
-    {
 
-        $window.history.back();
-    });
-    
- };
+            var postedEvent = {
+                title: $scope.event.title,
+                location: $scope.event.location,
+                date: "",
+                time: "",
+                description: $scope.event.description,
+                category: $scope.event.category
+            };
 
-}])
+            var d = $scope.event.date;
+            postedEvent.date = weekday[d.getDay()] + ", " + month[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+            postedEvent.time = $scope.event.time.toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+            console.log(postedEvent);
 
+            adminRef.child('buildingEvents').push(postedEvent).then(function(success)
+            {
+                $scope.event = {
+                    category:"",
+                    date:"",
+                    location:"",
+                    time:"",
+                    description:"",
+                    title:""
+                    };
+                $scope.errorpopup = '';
+                $scope.eventsubmitted = true;
+                $timeout(function () {$scope.$apply();});
+                $timeout(function() {$scope.eventsubmitted = false;$scope.$apply();}, 3000);
+            })
 
+};
 
-.controller('constructionCtrl', ['$scope',
-    // a verbose line seperator between the top construction section and the function for the controller below
+function question(title, choices) 
+{
+    this.title = title;
+    this.choices = choices;
+}
 
- function($scope) {
+$scope.survey = {
+    title: "",
+    description:"",
+    questions:[]
+};
+
+function answer(text)
+{
+    this.answer = text;
+}
+
+$scope.addQuestion = function()
+{
+    $scope.survey.questions.push(new question("",[]));
+    console.log($scope.survey);
+
+};
+
+$scope.removeQuestion = function()
+{
+    $scope.survey.questions.pop();
+    console.log($scope.survey);
+
+};
+
+$scope.addCheckbox = function(index)
+{
+    $scope.survey.questions[index].choices.push(new answer(""));
+    console.log($scope.survey);
+
+};
+
+$scope.removeCheckbox = function(index)
+{
+    $scope.survey.questions[index].choices.pop();
+    console.log($scope.survey);
+
+};
+
+$scope.submitSurvey = function()
+{
+    console.log("survey submit begin");
+        
+        if($scope.survey.title == "" || $scope.survey.title == " ")
+        {
+            $scope.errorpopup = "Please enter a title";
+            return;
+        }
+        if($scope.survey.description == "" || $scope.survey.description == " ")
+        {
+            $scope.errorpopup = "Please enter a description";
+            return;
+        }
+        if($scope.survey.questions.length == 0)
+        {
+            $scope.errorpopup = "You must have at least 1 question to submit";
+            return;
+        }
+
+        for(var i in $scope.survey.questions)
+        {
+            if($scope.survey.questions[i].title == "" || $scope.survey.questions[i].title == " ")
+            {
+                $scope.errorpopup = "Please fill out all fields";
+                return;
+            }
+            if($scope.survey.questions[i].choices.length < 2)
+            {
+                $scope.errorpopup = "You must have at least 2 answers per question";
+                return;
+            }
+            else
+            {
+                for(var j in $scope.survey.questions[i].choices)
+                {
+                    if($scope.survey.questions[i].choices[j].answer == "" 
+                        || $scope.survey.questions[i].choices[j].answer == " ")
+                    {
+                        $scope.errorpopup = "Please fill out all fields";
+                        return;
+                    }
+                }
+            }
+        }
+
+        console.log("Submitting survey");
+
+            /*var postedEvent = {
+                title: $scope.event.title,
+                location: $scope.event.location,
+                date: "",
+                time: "",
+                description: $scope.event.description,
+                category: $scope.event.category
+            };
+
+            var d = $scope.event.date;
+            postedEvent.date = weekday[d.getDay()] + ", " + month[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+            postedEvent.time = $scope.event.time.toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+            console.log(postedEvent);
+            */
+            adminRef.child('Surveys').push($scope.survey).then(function(success)
+            {
+                $scope.survey = {
+                title: "",
+                description:"",
+                questions:[]
+            };
+                $scope.errorpopup = '';
+                $scope.surveysubmitted = true;
+                $timeout(function () {$scope.$apply();});
+                $timeout(function() {$scope.surveysubmitted = false;$scope.$apply();}, 3000);
+            })
+
+};
 
 
 }])
